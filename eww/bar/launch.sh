c9e244e35bc80bce768da183289f692be6183428
TailR <132870183+Tail-R@users.noreply.github.com>
2023-08-31 11:44:09 +0900

    Add files via upload

---
+++ eww/bar/launch.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+ewwPath="$HOME/.config/eww/bar"
+tmpPath="$HOME/.cache/eww/bar"
+
+windows="
+            window_bg
+            window_start
+            window_workspace
+            window_activewindow
+            window_music 
+            
+            window_sysinfo 
+            window_battery
+            window_clock
+            window_monad
+
+            window_workspace_secondary
+            window_clock_secondary
+            window_battery_secondary 
+        "
+
+toggle() {
+    if [ -f $tmpPath ]; then
+        rm $tmpPath
+        eww -c $ewwPath close-all
+    else
+        touch $tmpPath
+        eww -c $ewwPath open-many $windows
+    fi
+}
+
+if [ "$1" == "--open-all" ]; then
+    touch $tmpPath
+    eww -c $ewwPath open-many $windows
+elif [ "$1" == "--toggle" ]; then
+    toggle
+fi
+
+
+
+
+
+
+
