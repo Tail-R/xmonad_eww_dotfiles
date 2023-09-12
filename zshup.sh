5c30347a84a3e151f94a9795a2c0f70a20fca85d
TailR <132870183+Tail-R@users.noreply.github.com>
2023-09-13 02:15:13 +0900

    Add files via upload

---
+++ zshup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Install zsh                                                                      
+sudo pacman -S zsh zsh-completions grml-zsh-config zsh-syntax-highlighting zsh-auto
+sudo cp /etc/zsh/zshrc $HOME/.zshrc                                                
+$(cp $HOME/xmonad_eww_dotfiles/zshenv $HOME/.zshenv)                               
+$(cp $HOME/xmonad_eww_dotfiles/zshrc.local $HOME/.zshrc.local) 
+exec zsh
