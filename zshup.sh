833643a14112c65f8d6a870c013dac0124c394ab
TailR <132870183+Tail-R@users.noreply.github.com>
2023-11-01 09:15:33 +0900

    Delete zshup.sh

--- zshup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install zsh                                                                      
-sudo pacman -S zsh zsh-completions grml-zsh-config zsh-syntax-highlighting zsh-autosuggestions
-sudo cp /etc/zsh/zshrc $HOME/.zshrc                                                
-$(cp $HOME/xmonad_eww_dotfiles/zshenv $HOME/.zshenv)                               
-$(cp $HOME/xmonad_eww_dotfiles/zshrc.local $HOME/.zshrc.local) 
-chsh -s $(which zsh)
-exec zsh
